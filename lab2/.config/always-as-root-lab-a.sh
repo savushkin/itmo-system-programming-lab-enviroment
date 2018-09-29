6b3dac71eb8216d27ecd123ff357c26c6e71aa13
Ivan Savushkin <ivan@savushkin.me>
2018-09-29 14:40:33 +0300

    superfix

--- lab2/.config/always-as-root-lab-a.sh
+++ lab2/.config/always-as-root-lab-a.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ifconfig gre create
-ifconfig gre0 tunnel 192.168.56.101 192.168.56.103
-ifconfig gre0 192.168.254.1/32 192.168.254.2 netmask 255.255.255.0
-ifconfig gre0 up
+#ifconfig gre create
+#ifconfig gre0 tunnel 192.168.56.101 192.168.56.103
+#ifconfig gre0 192.168.254.1/32 192.168.254.2 netmask 255.255.255.0
+#ifconfig gre0 up
--- lab2/.config/always-as-root-lab-b.sh
+++ lab2/.config/always-as-root-lab-b.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ifconfig gre create
-ifconfig gre0 tunnel 192.168.56.103 192.168.56.101
-ifconfig gre0 192.168.254.2/32 192.168.254.1 netmask 255.255.255.0
-ifconfig gre0 up
+#ifconfig gre create
+#ifconfig gre0 tunnel 192.168.56.103 192.168.56.101
+#ifconfig gre0 192.168.254.2/32 192.168.254.1 netmask 255.255.255.0
+#ifconfig gre0 up
